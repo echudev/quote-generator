89bcfe84db1ac960f4c19e37e17ffead7c926149
Ezequiel <ezequiel.maranda@gmail.com>
2022-03-28 23:38:24 -0300

    first commit

---
+++ src/componentes/GetData/GetData.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import './GetData.css'
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import axios from 'axios';
+
+
+export default function GetData() {
+
+    // //conecta a la API usando FETCH
+    // const [data, setData] = useState("");
+    // const getRandomQuote = async () => {
+    //     try {
+    //         const response = await fetch('https://api.adviceslip.com/advice');
+    //         const json = await response.json();
+    //         setData(json.slip.advice)
+    //     }
+    //     catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    //Conecto a la API utilizando AXIOS
+    const [dataAxios, setDataAxios] = useState("");
+    const getRandomQuoteAxios = async () => {
+        try {
+            const response = await axios.get('https://api.adviceslip.com/advice');
+            setDataAxios(response.data.slip.advice)
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getRandomQuoteAxios();
+    }, []);
+
+    const [alertClassName, setAlertClassName] = useState ("Alert")
+    const copyQuote = () => {
+        navigator.clipboard.writeText(dataAxios);
+
+        // alerta de copiado    
+        setAlertClassName ("Alert showAlert");
+        setTimeout(() => {
+            setAlertClassName ("Alert");
+        }, 1000);
+    }
+
+
+    return (
+        <>
+            <div className="quote_container">
+                <p className="quote">{dataAxios}</p>
+            </div>
+
+            <div className="button_container">
+                <Button variant="contained" color="secondary" className="button" onClick={getRandomQuoteAxios}>Refresh</Button>
+                <Button variant="outlined" color="secondary" className="button" onClick={copyQuote}>Copy</Button>
+                <Alert severity="success"className={alertClassName}>Saved to Clipboard</Alert>
+            </div>
+        </>
+    )
+}
+
+
+
